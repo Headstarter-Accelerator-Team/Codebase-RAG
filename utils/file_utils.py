c4c3f500b1f39c3df2f1706b3a9e34729e637f86
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-11-29 19:02:44 -0800

    Enhancement: Added comments/documentation to '/utils/file_utils.py'.

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -2,13 +2,31 @@ import os
 from utils.python_parser import PythonParser
 
 def read_file(path):
+    """
+    Reads the content of a file.
+
+    Args:
+        path (str): The path to the file.
+
+    Returns:
+        str: The content of the file.
+    """
     with open(path, 'r') as file:
         content = file.read()
-        # print(content)
         return content
     
 
 def list_files_recursive(path, files):
+    """
+    Recursively lists all files in a directory, excluding .git directories.
+
+    Args:
+        path (str): The path to the directory.
+        files (list): The list to store file information.
+
+    Returns:
+        None
+    """
     for entry in os.listdir(path):
         if entry == ".git":
             continue
@@ -25,8 +43,16 @@ def list_files_recursive(path, files):
     
 
 def get_file_extension(filename):
+    """
+    Gets the file extension of a given filename.
+
+    Args:
+        filename (str): The name of the file.
+
+    Returns:
+        str: The file extension.
+    """
     split_tup = os.path.splitext(filename)
-    # extract the file name and extension
     file_name = split_tup[0]
     file_extension = split_tup[1]
     print("File Name: ", file_name)
@@ -35,6 +61,15 @@ def get_file_extension(filename):
 
 
 def process_python_files(file):
+    """
+    Processes a Python file by converting its content to an AST representation.
+
+    Args:
+        file (dict): A dictionary containing the file's source path and content.
+
+    Returns:
+        dict: A dictionary containing the file's source path and its AST representation.
+    """
     parser = PythonParser()
     tree = parser.convert_python_to_ast(file['content'])['ast_representation']
 
